184c00f89c07cc0336a522f11c708e3028e605f8
KreamsOnline <kreamsonline@kreamsoinesimac.home>
2021-04-25 17:26:42 +0100

    ...

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const gameArea = document.querySelector('.gameArea');
 document.addEventListener('DOMContentLoaded', init);
-const game = {row: 5, col: 5};
+const game = { 
+    row: 9, 
+    col: 9,
+    arr: [],
+    ani: {},
+    max: 5,
+    actives: 0,
+    inPlay: false
+};
 function init() {
     gameArea.innerHTML = '';
     const main = createNewElement(gameArea, 'div', '', 'gridContainer')
     
     buildGrid(main);
+    game.ani = requestAnimationFrame(startGame);
+}
+
+function startGame() {
+    if(game.actives < game.max){
+        setActive();
+    }
+
+    game.ani = requestAnimationFrame(startGame)
+}
+
+function setActive() {
+    game.actives++;
+    const sel = Math.floor(Math.random()*game.arr.length);
+    const myEl = game.arr[sel];
+    const timer = Math.floor(Math.random()*4000)+1000;
+    myEl.classList.add('active');
+    setTimeout(removeActive, timer, myEl);
+}
+
+function removeActive(myEl) {
+    console.log(myEl);
+    myEl.classList.remove('active');
+    game.actives--;
 }
 
 function buildGrid(main) {
@@ -18,7 +50,8 @@ function buildGrid(main) {
         for (let x = 0; x < game.col; x++) {
             if(y == 0) {dim.x += '  auto ';}
             const cell = y*game.col+x+1;
-            createNewElement(main, 'div', y+1, 'grid-item');
+            const el = createNewElement(main, 'div', y+1, 'grid-item');
+            game.arr.push(el);
         }
     }
     main.style.gridTemplateColumns = dim.x;
